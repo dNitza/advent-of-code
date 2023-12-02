eac5934e03d72858c34abecb6b16c54f0b11646a
Daniel Nitsikopoulos <daniel@dnitza.com>
2023-12-02 16:40:37 +1100

    Day 2

--- 2023/02/lib/puzzle.rb
+++ 2023/02/lib/puzzle.rb
@@ -1,10 +1,70 @@
 class Puzzle
-
   def part1(input)
+    games = build_games(input)
 
+    result = games.reject do |game|
+      game[:rounds].any? { |round|
+        round[:red] > 12 ||
+          round[:green] > 13 ||
+          round[:blue] > 14
+      }
+    end
+
+    result.map{|r| r[:id]}.inject(:+)
   end
 
   def part2(input)
+    games = build_games(input)
 
-  end 
+    games.map do |game|
+      min_red = game[:rounds]
+                  .map{|r| r[:red].zero? ? nil : r[:red] }
+                  .compact
+                  .max
+      min_green = game[:rounds]
+                    .map{|r| r[:green].zero? ? nil : r[:green] }
+                    .compact
+                    .max
+      min_blue = game[:rounds]
+                   .map{|r| r[:blue].zero? ? nil : r[:blue] }
+                   .compact
+                   .max
+
+      [min_red, min_green, min_blue].inject(:*)
+    end.inject(:+)
+  end
+
+  private
+
+  def build_games(input)
+    # game = {
+    #   id: 0,
+    #   rounds: [
+    #     red: 0,
+    #     green: 0,
+    #     blue: 0,
+    #   ]
+    # }
+
+    input.map do |line|
+      id = line.scan(/(\d+)[:]/).flatten.first.to_i
+      game = line.gsub("Game #{id}: ", "").split(";")
+      rounds = game.map do |reveal|
+        red = reveal.scan(/(\d+) red/).flatten.first&.to_i || 0
+        green = reveal.scan(/(\d+) green/).flatten.first&.to_i || 0
+        blue = reveal.scan(/(\d+) blue/).flatten.first&.to_i || 0
+
+        {
+          red: red,
+          green: green,
+          blue: blue
+        }
+      end
+
+      {
+        id: id,
+        rounds: rounds
+      }
+    end
+  end
 end
--- 2023/02/spec/puzzle_spec.rb
+++ 2023/02/spec/puzzle_spec.rb
@@ -5,10 +5,26 @@ RSpec.describe Puzzle do
   subject { described_class.new }
 
   it "part 1" do
+    input = <<-INPUT
+      Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+      Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+      Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+      Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+      Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+    INPUT
 
+    expect(subject.part1(input.split("\n").map(&:chomp))).to eq 8
   end
 
   it "part 2" do
+    input = <<-INPUT
+      Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+      Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+      Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+      Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+      Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+    INPUT
 
+    expect(subject.part2(input.split("\n").map(&:chomp))).to eq 2286
   end
 end
